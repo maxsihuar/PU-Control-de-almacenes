13ad0408832f075455eae4b55f968ef44341d2fd
Marco <marcoantonio123.xyz@gmail.com>
2025-09-29 10:48:04 -0500

    Creación de menú principal y menú secundario
    estará bien ps

--- src/views/view_menu.rs
+++ src/views/view_menu.rs
@@ -1,6 +1,22 @@
+use std::io;
 pub fn menu_principal(){
     println!("--- Menu Principal ---");
     println!("1. Categorias");
-    println!("2. Productos");
-    println!("3. Salir");
+    println!("2. Artículos");
+    println!("3. Proveedores");
+    println!("4. Entradas");
+    println!("5. Detalles de Entrada");
+    println!("6. Salidas");
+    println!("7. Detalles de Salida");
+    println!("8. Salir");
 } 
+pub fn menu_secundario(texto: String){
+    let base = "Menu ";
+    let texto_final: String = base.to_string() + &texto;
+    println!("---{}---",texto_final);
+    println!("1. Agregar");
+    println!("2. Modificar");
+    println!("3. Eliminar");
+    println!("4. Listar");
+    println!("5. Salir");                                              
+}
