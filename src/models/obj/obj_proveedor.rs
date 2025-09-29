82ce6713989137af235eed578034b8046842a802
max98 <75954578+maxsihuar@users.noreply.github.com>
2025-09-29 10:52:58 -0500

    Create obj_proveedor.rs

---
+++ src/models/obj/obj_proveedor.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+/// Crea un nuevo diccionario (`HashMap`) para almacenar proveedores.
+///
+/// # Retorno
+/// Retorna un `HashMap<String, (String, String, String, String)>` donde:
+///
+/// - La **clave** (`String`) representa el codigo del proveedor.
+/// - El **valor** es una **tupla** con la siguiente estructura:
+///   1. `String` — Razon social del proveedor.
+///   2. `String` — RUC del proveedor.
+///   4. `String` — Direccion-.
+///   5. `String` — Ciudad.
+
+
+pub fn crear_proveedor() -> HashMap<String, (String, String, u32, String, String)> {
+    return HashMap::new()
+}
