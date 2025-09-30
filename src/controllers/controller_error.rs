6d4315f02b43aa275c06699cc9cd7ddd82298787
Max <maxsihuar98@gmail.com>
2025-09-30 00:14:34 -0500

    add documention in controller_error

--- src/controllers/controller_error.rs
+++ src/controllers/controller_error.rs
@@ -1,13 +1,28 @@
 use crate::src::views::view_error;
 
-pub fn controller_error_no_existencia(){
+/// Controlador que gestiona el error cuando un código no existe.
+///
+/// # Descripción
+/// Llama a la función correspondiente en la capa de **vista (`view_error`)**
+/// para mostrar un mensaje de error de "código no existente".
+pub fn controller_error_no_existencia() {
     view_error::error_no_existencia();
 }
 
-pub fn controller_error_existencia(){
+/// Controlador que gestiona el error cuando un código ya existe.
+///
+/// # Descripción
+/// Llama a la función de la capa de **vista (`view_error`)**
+/// para mostrar un mensaje de error de "código ya existente".
+pub fn controller_error_existencia() {
     view_error::error_existencia();
 }
 
-pub fn controller_error_RUC_tamaño(){
+/// Controlador que gestiona el error cuando el RUC no tiene el tamaño correcto.
+///
+/// # Descripción
+/// Invoca la función de la capa de **vista (`view_error`)**
+/// para mostrar el mensaje de error de "RUC debe tener 11 dígitos".
+pub fn controller_error_RUC_tamaño() {
     view_error::error_RUC_tamaño();
 }
