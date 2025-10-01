2579437a6cc3826b1d5cd45f4b5a6dc5732291f7
Max <maxsihuar98@gmail.com>
2025-09-30 22:12:58 -0500

    commit example

--- src/controllers/controller_articulos.rs
+++ src/controllers/controller_articulos.rs
@@ -4,9 +4,9 @@ use std::collections::HashMap;
 
 pub fn leer_datos_articulo() -> (String, (String, String, u32)) {
     // Lógica para leer datos del artículo
-    let codigo = utils::utils_leer::leer_string()
-    let descripcion = utils::utils_leer::leer_string()
-    let precio = utils::utils_leer::leer_u32()
+    let codigo = utils::utils_leer::leer_string();
+    let descripcion = utils::utils_leer::leer_string();
+    let precio = utils::utils_leer::leer_u32();
 
     return (codigo, (descripcion, categoria, precio));
 
