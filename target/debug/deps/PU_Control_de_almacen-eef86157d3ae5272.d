179da527ce10599cf9ecba6941287d4bcf11313e
Marco <marcoantonioaroniperez@gmail.com>
2025-10-01 09:51:18 -0500

    uwu

--- target/debug/deps/PU_Control_de_almacen-eef86157d3ae5272.d
+++ target/debug/deps/PU_Control_de_almacen-eef86157d3ae5272.d
@@ -1,5 +1,44 @@
-C:\Users\USUARIO\OneDrive\Documentos\Github\PU-Control-de-almacenes\target\debug\deps\PU_Control_de_almacen-eef86157d3ae5272.d: src\main.rs
+C:\Users\USUARIO\OneDrive\Documentos\Github\PU-Control-de-almacenes\target\debug\deps\PU_Control_de_almacen-eef86157d3ae5272.d: src\main.rs src\controllers\mod.rs src\controllers\controller_main.rs src\controllers\controller_categoria.rs src\controllers\controller_error.rs src\controllers\controller_articulos.rs src\controllers\controller_detalle_entrada.rs src\controllers\controller_detalle_salida.rs src\controllers\controller_entrada.rs src\controllers\controller_proveedores.rs src\controllers\controller_salida.rs src\controllers\controller_reporte.rs src\models\mod.rs src\models\obj\mod.rs src\models\obj\obj_articulo.rs src\models\obj\obj_categoria.rs src\models\obj\obj_cliente.rs src\models\obj\obj_proveedor.rs src\models\obj\obj_entrada.rs src\models\obj\obj_salida.rs src\models\obj\obj_detalle_entrada.rs src\models\obj\obj_detalle_salida.rs src\models\services\mod.rs src\models\services\services_agregar.rs src\models\services\services_listar.rs src\models\services\services_eliminar.rs src\models\services\services_modificar.rs src\models\services\services_reportes.rs src\models\utils\mod.rs src\models\utils\utils_validaciones.rs src\models\utils\utils_leer.rs src\views\mod.rs src\views\view_menu.rs src\views\utils\mod.rs src\views\utils\utils_view.rs src\views\view_error.rs src\views\view_leer.rs src\views\view_listar.rs src\tests\mod.rs src\tests\test1.rs
 
-C:\Users\USUARIO\OneDrive\Documentos\Github\PU-Control-de-almacenes\target\debug\deps\libPU_Control_de_almacen-eef86157d3ae5272.rmeta: src\main.rs
+C:\Users\USUARIO\OneDrive\Documentos\Github\PU-Control-de-almacenes\target\debug\deps\libPU_Control_de_almacen-eef86157d3ae5272.rmeta: src\main.rs src\controllers\mod.rs src\controllers\controller_main.rs src\controllers\controller_categoria.rs src\controllers\controller_error.rs src\controllers\controller_articulos.rs src\controllers\controller_detalle_entrada.rs src\controllers\controller_detalle_salida.rs src\controllers\controller_entrada.rs src\controllers\controller_proveedores.rs src\controllers\controller_salida.rs src\controllers\controller_reporte.rs src\models\mod.rs src\models\obj\mod.rs src\models\obj\obj_articulo.rs src\models\obj\obj_categoria.rs src\models\obj\obj_cliente.rs src\models\obj\obj_proveedor.rs src\models\obj\obj_entrada.rs src\models\obj\obj_salida.rs src\models\obj\obj_detalle_entrada.rs src\models\obj\obj_detalle_salida.rs src\models\services\mod.rs src\models\services\services_agregar.rs src\models\services\services_listar.rs src\models\services\services_eliminar.rs src\models\services\services_modificar.rs src\models\services\services_reportes.rs src\models\utils\mod.rs src\models\utils\utils_validaciones.rs src\models\utils\utils_leer.rs src\views\mod.rs src\views\view_menu.rs src\views\utils\mod.rs src\views\utils\utils_view.rs src\views\view_error.rs src\views\view_leer.rs src\views\view_listar.rs src\tests\mod.rs src\tests\test1.rs
 
 src\main.rs:
+src\controllers\mod.rs:
+src\controllers\controller_main.rs:
+src\controllers\controller_categoria.rs:
+src\controllers\controller_error.rs:
+src\controllers\controller_articulos.rs:
+src\controllers\controller_detalle_entrada.rs:
+src\controllers\controller_detalle_salida.rs:
+src\controllers\controller_entrada.rs:
+src\controllers\controller_proveedores.rs:
+src\controllers\controller_salida.rs:
+src\controllers\controller_reporte.rs:
+src\models\mod.rs:
+src\models\obj\mod.rs:
+src\models\obj\obj_articulo.rs:
+src\models\obj\obj_categoria.rs:
+src\models\obj\obj_cliente.rs:
+src\models\obj\obj_proveedor.rs:
+src\models\obj\obj_entrada.rs:
+src\models\obj\obj_salida.rs:
+src\models\obj\obj_detalle_entrada.rs:
+src\models\obj\obj_detalle_salida.rs:
+src\models\services\mod.rs:
+src\models\services\services_agregar.rs:
+src\models\services\services_listar.rs:
+src\models\services\services_eliminar.rs:
+src\models\services\services_modificar.rs:
+src\models\services\services_reportes.rs:
+src\models\utils\mod.rs:
+src\models\utils\utils_validaciones.rs:
+src\models\utils\utils_leer.rs:
+src\views\mod.rs:
+src\views\view_menu.rs:
+src\views\utils\mod.rs:
+src\views\utils\utils_view.rs:
+src\views\view_error.rs:
+src\views\view_leer.rs:
+src\views\view_listar.rs:
+src\tests\mod.rs:
+src\tests\test1.rs:
